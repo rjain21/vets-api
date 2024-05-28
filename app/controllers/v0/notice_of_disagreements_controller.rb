e2e2279f1bc35d405898f8e37f72e69860613d37
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-05-28 19:35:31 -0400

    update v0 controller

--- app/controllers/v0/notice_of_disagreements_controller.rb
+++ app/controllers/v0/notice_of_disagreements_controller.rb
@@ -3,39 +3,64 @@
 module V0
   class NoticeOfDisagreementsController < AppealsBaseController
     service_tag 'board-appeal'
+
     def create
-      nod_response_body = AppealSubmission.submit_nod(
-        current_user: @current_user,
-        request_body_hash:,
-        decision_review_service:
-      )
-      render json: nod_response_body
+      raise 'Must pass in a version of the DecisionReview Service' if decision_review_service.nil?
+
+      ActiveRecord::Base.transaction do
+        InProgressForm.form_for_user('10182', @current_user)&.destroy!
+        enqueue_uploads
+        render json: nod_response_body
+      end
     rescue => e
       request = begin
         { body: request_body_hash }
       rescue
         request_body_debug_data
       end
-
-      log_exception_to_personal_information_log(
-        e, error_class: error_class(method: 'create', exception_class: e.class), request:
-      )
+      handle_exception(method: 'create', exception: e, options: { request: request })
       raise
     end
 
     def show
       render json: decision_review_service.get_notice_of_disagreement(params[:id]).body
     rescue => e
-      log_exception_to_personal_information_log(
-        e, error_class: error_class(method: 'show', exception_class: e.class), id: params[:id]
-      )
+      handle_exception(method: 'show', exception: e, options: { id: params[:id] })
       raise
     end
 
     private
 
-    def error_class(method:, exception_class:)
-      "#{self.class.name}##{method} exception #{exception_class} (NOD)"
+    def enqueue_uploads
+      uploads_arr = request_body_hash.delete('nodUploads') || []
+      appeal_submission.enqueue_uploads(uploads_arr, @current_user, 'v1')
+    end
+
+    def appeal_submission
+      @appeal_submission ||= AppealSubmission.create(appeal_submission_params)
+    end
+
+    def appeal_submission_params
+      {
+        type_of_appeal: 'NOD',
+        user_uuid: @current_user.uuid,
+        user_account: @current_user.user_account,
+        board_review_option: request_body_hash['data']['attributes']['boardReviewOption'],
+        upload_metadata: decision_review_service.class.file_upload_metadata(@current_user),
+        submitted_appeal_uuid: nod_response_body.dig('data', 'id')
+      }
+    end
+
+    def nod_response_body
+      @nod_response_body ||= decision_review_service.create_notice_of_disagreement(
+        request_body: request_body_hash,
+        user: @current_user
+      ).body
+    end
+
+    def handle_exception(method:, exception:, options:)
+      error_class = "#{self.class.name}##{method} exception #{exception.class} (NOD)"
+      log_exception_to_personal_information_log(exception, error_class: error_class, **options)
     end
   end
 end
--- app/models/appeal_submission.rb
+++ app/models/appeal_submission.rb
@@ -11,32 +11,6 @@ class AppealSubmission < ApplicationRecord
 
   has_many :appeal_submission_uploads, dependent: :destroy
 
-  def self.submit_nod(request_body_hash:, current_user:, decision_review_service: nil, version_number: 'v1')
-    ActiveRecord::Base.transaction do
-      raise 'Must pass in a version of the DecisionReview Service' if decision_review_service.nil?
-
-      appeal_submission = new(type_of_appeal: 'NOD',
-                              user_uuid: current_user.uuid,
-                              user_account: current_user.user_account,
-                              board_review_option: request_body_hash['data']['attributes']['boardReviewOption'],
-                              upload_metadata: decision_review_service.class.file_upload_metadata(current_user))
-
-      uploads_arr = request_body_hash.delete('nodUploads') || []
-      nod_response_body = decision_review_service.create_notice_of_disagreement(request_body: request_body_hash,
-                                                                                user: current_user)
-                                                 .body
-      appeal_submission.submitted_appeal_uuid = nod_response_body.dig('data', 'id')
-      appeal_submission.save!
-
-      # Clear in-progress form if submit was successful
-      InProgressForm.form_for_user('10182',
-                                   current_user)&.destroy!
-
-      appeal_submission.enqueue_uploads(uploads_arr, current_user, version_number)
-      nod_response_body
-    end
-  end
-
   def enqueue_uploads(uploads_arr, _user, version_number)
     uploads_arr.each do |upload_attrs|
       asu = AppealSubmissionUpload.create!(decision_review_evidence_attachment_guid: upload_attrs['confirmationCode'],
@@ -46,3 +20,42 @@ class AppealSubmission < ApplicationRecord
     end
   end
 end
+
+# => [{"name"=>"my_file.pdf", "confirmationCode"=>"aaad6f52-2e85-43d4-a5a3-1d9cb4e482a0"}]
+
+
+
+# ================================================================================
+# An HTTP request has been made that VCR does not know how to handle:
+#   POST https://sandbox-api.va.gov/services/appeals/v1/decision_reviews/notice_of_disagreements
+
+# VCR is currently using the following cassette:
+#   - /Users/steven.cumming/workspace/va.gov/vets-api/spec/support/vcr_cassettes/decision_review/NOD-CREATE-RESPONSE-200.yml
+#     - :record => :once
+#     - :match_requests_on => [:method, :uri]
+
+# Under the current configuration VCR can not find a suitable HTTP interaction
+# to replay and is prevented from recording new requests. There are a few ways
+# you can deal with this:
+
+#   * If you're surprised VCR is raising this error
+#     and want insight about how VCR attempted to handle the request,
+#     you can use the debug_logger configuration option to log more details [1].
+#   * You can use the :new_episodes record mode to allow VCR to
+#     record this new request to the existing cassette [2].
+#   * If you want VCR to ignore this request (and others like it), you can
+#     set an `ignore_request` callback [3].
+#   * The current record mode (:once) does not allow new requests to be recorded
+#     to a previously recorded cassette. You can delete the cassette file and re-run
+#     your tests to allow the cassette to be recorded with this request [4].
+#   * The cassette contains an HTTP interaction that matches this request,
+#     but it has already been played back. If you wish to allow a single HTTP
+#     interaction to be played back multiple times, set the `:allow_playback_repeats`
+#     cassette option [5].
+
+# [1] https://www.relishapp.com/vcr/vcr/v/6-2-0/docs/configuration/debug-logging
+# [2] https://www.relishapp.com/vcr/vcr/v/6-2-0/docs/record-modes/new-episodes
+# [3] https://www.relishapp.com/vcr/vcr/v/6-2-0/docs/configuration/ignore-request
+# [4] https://www.relishapp.com/vcr/vcr/v/6-2-0/docs/record-modes/once
+# [5] https://www.relishapp.com/vcr/vcr/v/6-2-0/docs/request-matching/playback-repeats
+# ================================================================================
