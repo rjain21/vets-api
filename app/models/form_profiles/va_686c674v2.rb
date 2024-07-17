dc896bd23e89143a574d3a1fd27d9eaf5821cc90
Evan Smith <evan.smith@oddball.io>
2024-07-17 13:11:31 -0400

    prefill experiment

--- app/models/form_profiles/va_686c674v2.rb
+++ app/models/form_profiles/va_686c674v2.rb
@@ -48,5 +48,12 @@ class FormProfiles::VA686c674v2 < FormProfile
         ).merge(country_name: mailing_address.country_code_iso3)
       )
     end
+
+    def va_file_number_last_four
+        response = BGS::People::Request.new.find_person_by_participant_id(user:)
+        (
+          response.file_number.presence || user.ssn.presence
+        )&.last(4)
+      end
   end
   
